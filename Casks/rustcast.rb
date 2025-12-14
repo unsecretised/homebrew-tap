d7f19885697101e72616f6056c01549520c5941b
unsecretised <no1umang@gmail.com>
2025-12-14 11:58:41 +0800

    Update sha256 checksum

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -2,7 +2,7 @@ cask "rustcast" do
   arch arm: "aarch64-apple-darwin"
 
   version "0.1.1"
-  sha256 arm: "sha256:d216bcde5d0a42646d922bce4f6bac33fc9b7f6afc354e92e31602657d3b52d4"
+  sha256 arm: "d216bcde5d0a42646d922bce4f6bac33fc9b7f6afc354e92e31602657d3b52d4"
 
   url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/rustcast-#{arch}.app.zip",
       verified: "github.com/unsecretised/rustcast/"
