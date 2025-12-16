63fc288cf194f4585e7e3889834ba6385808efd1
unsecretised <no1umang@gmail.com>
2025-12-16 17:15:09 +0800

    Update cask to support intel macs

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -1,11 +1,15 @@
 cask "rustcast" do
-  arch arm: "aarch64-apple-darwin"
+  arch arm: "aarch64-apple-darwin",
+       intel: "x86_64-apple-darwin"
 
-  version "0.1.2.1"
-  sha256 arm: "afc5bcde831253a3ac7b7399b6695d0a58c6c516117383a026db0945f4faaea6"
+  version "0.1.3.1"
+
+  sha256 arm:   "0e64ffdcd2efef60281962d2c2aa93ed744e51357b5b80f4c52c23a047455945",
+         intel: "bf6d0a6d36985c990dfba9fac9a531fc8e0b5349f068991b81a5d2d7b53dbbe9"
 
   url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/rustcast-#{arch}.app.zip",
       verified: "github.com/unsecretised/rustcast/"
+
   name "Rustcast"
   desc "Google search for your system"
   homepage "https://github.com/unsecretised/rustcast"
@@ -16,7 +20,7 @@ cask "rustcast" do
 
   postflight do
     system_command "/usr/bin/xattr",
-      args: ["-cr", "/Applications/Rustcast.app/"],
-      sudo: false
+                   args: ["-cr", "/Applications/Rustcast.app/"],
+                   sudo: false
   end
 end
