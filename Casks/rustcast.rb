15561aa315c96e244f82f017b5472f3a83bd99f5
unsecretised <no1umang@gmail.com>
2025-12-23 17:59:22 +0800

    Update Version

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -2,10 +2,10 @@ cask "rustcast" do
   arch arm: "aarch64-apple-darwin",
        intel: "x86_64-apple-darwin"
 
-  version "0.1.6.1"
+  version "0.1.6.2"
 
-  sha256 arm:   "566d961f385b8b4fdba935d7cc5929b19cd7e2e7bb0a75b7485249b65a8d1e0f",
-         intel: "fcab1977d9628c17a6f4ad0119faadc3b879b8bcffe59bd7a351b9a0445baab5"
+  sha256 arm:   "95741aff55d7b63f0dcd34990b2ce7dd28df62902a486c05ad264a8515648e91",
+         intel: "cf2f76938cd4966edd12727a93580822a3095eaa47c4a74320e9e827951f2576"
 
   url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/rustcast-#{arch}.app.zip",
       verified: "github.com/unsecretised/rustcast/"
