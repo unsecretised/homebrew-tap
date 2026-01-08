a9bd342a119f03c5572dd3db36ea1e7c89e55bb8
unsecretised <no1umang@gmail.com>
2026-01-09 00:54:05 +0800

    Update version

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -1,8 +1,8 @@
 cask "rustcast" do
 
-  version "0.3.4-beta"
+  version "0.3.4"
 
-  sha256 "470661c6bbc90a6c58cc95cc059e5754e59c12ea1f4c278a27e0c58dac9057e3"
+  sha256 "b69d3da7b1f39a807f3551e53bd9bb79c81c45be72ac6a3ec06929b445d3702b"
 
   url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip",
       verified: "github.com/unsecretised/rustcast/"
