346d1b8239df23b39c182a9faa0e67b74e8c5307
unsecretised <no1umang@gmail.com>
2026-01-09 00:26:53 +0800

    Test Update Version

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -1,13 +1,10 @@
 cask "rustcast" do
-  arch arm: "aarch64-apple-darwin",
-       intel: "x86_64-apple-darwin"
 
-  version "0.3.3"
+  version "0.3.4-beta"
 
-  sha256 arm:   "5efd53ee5ad2479b3fe01cb51b47d027492540fd908b291ba90b8a77630e0fd1",
-         intel: "a6c3e50327222fa50febe5092cd0e72be1e5a96ec0dd6509bf716559f52533e2"
+  sha256 "470661c6bbc90a6c58cc95cc059e5754e59c12ea1f4c278a27e0c58dac9057e3",
 
-  url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/rustcast-#{arch}.app.zip",
+  url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip",
       verified: "github.com/unsecretised/rustcast/"
 
   name "Rustcast"
@@ -18,9 +15,4 @@ cask "rustcast" do
 
   app "Rustcast.app"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "/Applications/Rustcast.app/"],
-                   sudo: false
-  end
 end
