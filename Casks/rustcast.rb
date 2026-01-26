2dca7df07f7da4a3fc23fb7d522067c12a8e1eff
unsecretised <no1umang@gmail.com>
2026-01-26 09:48:40 +0800

    Update

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -4,15 +4,18 @@ cask "rustcast" do
 
   sha256 "63a13d2c533c029535c3ca376d9fb783b59a3fcb9116e0cfb37342710b5f7d5c"
 
-  url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip",
-      verified: "github.com/unsecretised/rustcast/"
+  url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip"
 
   name "Rustcast"
   desc "Utility tooling for your system"
   homepage "https://github.com/unsecretised/rustcast"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :big_sur"
 
   app "target/release/macos/Rustcast.app"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 end
