d7ec2983f23b2bc10bf7d9a58b44cbd945081d15
unsecretised <no1umang@gmail.com>
2026-04-25 09:08:35 +0800

    Update version

--- Casks/rustcast.rb
+++ Casks/rustcast.rb
@@ -1,10 +1,10 @@
 cask "rustcast" do
 
-  version "0.7.5"
+  version "0.7.6"
 
-  sha256 "ef1b51f802df1086415f0f687bc31f4dc757a248cc4dc1029157d3667a156b80"
+  sha256 "f02b117bf7c2f2c7709c13963c768896684fe7e4fbfd44892ad5c730ac522b78"
 
-  url "https://github.com/unsecretised/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip"
+  url "https://github.com/RustCastLabs/rustcast/releases/download/v#{version}/Rustcast-universal-macos.app.zip"
 
   name "Rustcast"
   desc "Utility tooling popup window for your system"
