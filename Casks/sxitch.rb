2e337ba0dc7d8ede56a731058a2e6d05cda44271
unsecretised <no1umang@gmail.com>
2026-06-27 19:15:28 +0800

    Update cask

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -4,16 +4,16 @@ cask "sxitch" do
 
   url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/Sxitch.app.zip"
   name "Sxitch"
-  desc "Tree based app switcher"
+  desc "Tree-based app switcher"
   homepage "https://sxitch.app/"
 
-  # The livecheck block can be completely removed since version is :latest, 
-  # but if you must keep it, Homebrew will ignore it anyway.
-
-  auto_updates false
   depends_on macos: :sequoia
 
   app "Sxitch.app"
 
-  zap trash: "~/Library/Application Support/Sxitch"
+  zap trash: [
+    "~/Library/Application Support/Sxitch",
+    "~/Library/Preferences/app.sxitch.plist",
+    "~/Library/Caches/app.sxitch",
+  ]
 end
