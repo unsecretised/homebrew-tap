eb4fdf68e1200eaeebec0ffc371f3c91ae62dd5f
unsecretised <no1umang@gmail.com>
2026-06-27 19:17:58 +0800

    Update commit

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -1,6 +1,6 @@
 cask "sxitch" do
   version :latest
-  sha256 :no_check
+  sha256 "a1739ec9a9b14dac687913acee3f80ac1a58f160de235ce126b1c8cd20d7b955"
 
   url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/Sxitch.app.zip"
   name "Sxitch"
