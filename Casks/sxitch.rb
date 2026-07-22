d497f448493cbcd363c334309b55ea16cb2bbace
unsecretised <no1umang@gmail.com>
2026-07-22 22:53:31 +0800

    Update sxitch livecheck

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -2,11 +2,16 @@ cask "sxitch" do
   version "1.2"
   sha256 "74643c98b4f7037a3bb798b8bf667d3880b644f78b0790e1b4c248a8727c72e8"
 
-  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/Sxitch.app.zip",
+  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch.app.zip",
       verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
   name "Sxitch"
   desc "Tree-based app switcher"
   homepage "https://sxitch.app/"
+
+  livecheck do
+    url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/version.txt"
+    regex(/^(\d+(?:\.\d+)+)$/i)
+  end
 
   depends_on macos: :sequoia
 
