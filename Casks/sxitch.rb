d5fefa554a6542f6915edc59d0ab9cb24bad6066
unsecretised <no1umang@gmail.com>
2026-07-22 22:13:51 +0800

    style fix

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -3,7 +3,7 @@ cask "sxitch" do
   sha256 "74643c98b4f7037a3bb798b8bf667d3880b644f78b0790e1b4c248a8727c72e8"
 
   url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/Sxitch.app.zip",
-    verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev"
+      verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
   name "Sxitch"
   desc "Tree-based app switcher"
   homepage "https://sxitch.app/"
