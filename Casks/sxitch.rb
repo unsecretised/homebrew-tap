c886382a4fa5f2f0c210dbcfb6fb894ea3f84f69
unsecretised <no1umang@gmail.com>
2026-08-21 23:01:29 +0800

    update sha256

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -1,6 +1,6 @@
 cask "sxitch" do
   version "1.4"
-  sha256 "0becd93f935cdc6c207d9e77624a9eaee8cffebbeb1e0f065cc0a5dcbce40005"
+  sha256 "1f9a2bad9d5e4f3b28a2fa1cd19df7efe1253004f2d5cfea8758bf80d3932004"
 
   url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch.app.zip",
       verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
