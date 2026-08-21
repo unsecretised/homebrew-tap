befac0bed5493829b2506c31073ffd17c2daaa41
unsecretised <no1umang@gmail.com>
2026-08-21 22:58:42 +0800

    update version

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -1,11 +1,11 @@
 cask "sxitch" do
-  version "1.3.1"
+  version "1.4"
   sha256 "0becd93f935cdc6c207d9e77624a9eaee8cffebbeb1e0f065cc0a5dcbce40005"
 
   url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch.app.zip",
       verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
   name "Sxitch"
-  desc "Tree-based app switcher"
+  desc "Tree-based macOS app switcher"
   homepage "https://sxitch.app/"
 
   livecheck do
