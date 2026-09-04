756778b4cdd8f393bbc5ad160f14e527f1170efe
Secretised <132474703+unsecretised@users.noreply.github.com>
2026-09-04 21:31:00 +0800

    Update sxitch.rb

--- Casks/sxitch.rb
+++ Casks/sxitch.rb
@@ -2,8 +2,7 @@ cask "sxitch" do
   version "1.4.2"
   sha256 "5312d4fcf68d31d6868e5c5e8d61268d0fa9c39c7452df110ce692efdb57e6c7"
 
-  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch.app.zip",
-      verified: "pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/"
+  url "https://pub-de38f9dc4d154d119d16caaa9a29824b.r2.dev/v#{version}/Sxitch.app.zip"
   name "Sxitch"
   desc "Tree-based app switcher"
   homepage "https://sxitch.app/"
